9dedb544cf01c54dd1fc6d8cd832072162569f6e
infun3 <infun3@gmail.com>
2018-12-10 06:30:59 +0000

    chatchart

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var express = require('express')
 var app = express()
 var Discord = require("discord.js");
-var ver ="0.86"
+var ver = "0.86"
 var mybot = new Discord.Client();
 var getJSON = require('get-json');
 
-mybot.login(process.env.TOKEN);//where BOT_TOKEN is the token of our bot
+//mybot.login(process.env.TOKEN);//where BOT_TOKEN is the token of our bot
 
-mybot.on("ready", function () {
-	console.log("Ready to begin! Serving in " + mybot.channels.length + " channels");
+mybot.on("ready", function() {
+    console.log("Ready to begin! Serving in " + mybot.channels.length + " channels");
 });
 
 mybot.on("message", function(message) {
@@ -18,47 +18,47 @@ mybot.on("message", function(message) {
                 mybot.reply(message, "she is currently not live");
             } else {
                 mybot.reply(message, "she is currently live");
-				mybot.sendMessage(message, "https://www.twitch.tv/lalicel");
+                mybot.sendMessage(message, "https://www.twitch.tv/lalicel");
             }
         });
     }
 });
 //long comands
-mybot.on("message", function(message){
+mybot.on("message", function(message) {
 
-            if( message.content === "!my_avatar" ){
+    if (message.content === "!my_avatar") {
 
         var usersAvatar = message.sender.avatarURL;
 
-        if(usersAvatar){
+        if (usersAvatar) {
             // user has an avatar
 
             mybot.reply(message, "your avatar can be found at " + usersAvatar);
 
-        }else{
+        } else {
             // user doesn't have an avatar
 
             mybot.reply(message, "you don't have an avatar!");
         }
 
-            }
+    }
 
-} );
+});
 
 
 
 //bot stuff
 mybot.on("message", function(message) {
-    if(message.content === "!help") {
-        mybot.reply(message, "**My current Commands** ```!about_bot, !live, !my_avatar, !twitch, !youtube, hype, cry```" );
+    if (message.content === "!help") {
+        mybot.reply(message, "**My current Commands** ```!about_bot, !live, !my_avatar, !twitch, !youtube, hype, cry```");
     }
 
 });
 
 mybot.on("message", function(message) {
-    if(message.content === "!about_bot") {
+    if (message.content === "!about_bot") {
         mybot.reply(message, "I was made by SloppierKitty7. I was written in nodejs and i'm runing on heroku and soon will be on github. I'm current on version is: " + ver + " @SloppierKitty7 should really update me");
-		mybot.sendFile(message, "http://i.imgur.com/izUfF1f.png");
+        mybot.sendFile(message, "http://i.imgur.com/izUfF1f.png");
 
     }
 });
@@ -66,13 +66,13 @@ mybot.on("message", function(message) {
 //links
 
 mybot.on("message", function(message) {
-    if(message.content === "!twitch") {
+    if (message.content === "!twitch") {
         mybot.reply(message, "https://www.twitch.tv/lalicel");
     }
 });
 
 mybot.on("message", function(message) {
-    if(message.content === "!youtube") {
+    if (message.content === "!youtube") {
         mybot.reply(message, "https://www.youtube.com/channel/UCVsd_WSEaW5oJTRZTI-2yBQ");
     }
 });
@@ -80,12 +80,12 @@ mybot.on("message", function(message) {
 //emots
 
 mybot.on("message", function(message) {
-    if(message.content === "hype") {
+    if (message.content === "hype") {
         mybot.sendFile(message, "http://i.imgur.com/zDPIzq4.png");
     }
 });
 mybot.on("message", function(message) {
-    if(message.content === "cry") {
+    if (message.content === "cry") {
         mybot.sendFile(message, "http://i.imgur.com/izUfF1f.png");
     }
 });
@@ -95,10 +95,10 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+    response.send('Hello World!')
 })
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+    console.log("Node app is running at localhost:" + app.get('port'))
 })
 
